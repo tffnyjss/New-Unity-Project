b4a6b2b84118354458b927feb6c9f77245cfb3a3
tffnyjss <jtiffany688@gmail.com>
2022-02-09 01:30:47 +0530

    GameOverNotSure

---
+++ Assets/Script/GameOver.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene("Menu Page");
+    }
+
+}
