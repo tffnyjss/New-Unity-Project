e13f9d5216b80e67bd2f16712bd9ce9f6f9cc71b
Andrea Alvares <andreaalvares1221@gmail.com>
2022-02-09 16:59:56 +0530

    GameOverMenu

---
+++ Assets/Script/CountDown.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountDown : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    [SerializeField] Image timeImage;
+    [SerializeField] Text timeText;
+    [SerializeField] float duration, currentTime;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panel.SetActive(false);
+        currentTime = duration;
+        timeText.text = currentTime.ToString();
+        StartCoroutine(TimeIEn());
+
+        
+    }
+    IEnumerator TimeIEn()
+    {
+        while(currentTime>=0)
+        {
+            timeImage.fillAmount = Mathf.InverseLerp(0, duration, currentTime);
+            timeText.text = currentTime.ToString();
+            yield return new WaitForSeconds(1f);
+            currentTime--;
+
+        }
+        OpenPanel();
+    }
+    void OpenPanel()
+    {
+        timeText.text = "" ;
+        panel.SetActive(true);
+    }
+    public void Retry(int sceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+    }
+    public void Home(int sceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+    }
+
+}
