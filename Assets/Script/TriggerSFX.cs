c15c9f645cd3c70d8fc62ba3132a48e908b37a58
Sharlene Misal <94185489+sharlenemisal@users.noreply.github.com>
2022-02-11 03:29:40 +0530

    Level 1,2,3 colliders

---
+++ Assets/Script/TriggerSFX.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSFX : MonoBehaviour
+{
+    public AudioSource playSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        playSound.Play();
+    }
+}
